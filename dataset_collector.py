37ffe658f9358475c618fe30f9d8085fcfcfd8fd
Plusharky <Plusharky@PLUSHARKY-PC>
2023-10-26 22:52:31 +0300

    Добавление асинхронности в код

--- dataset_collector.py
+++ dataset_collector.py
@@ -3,6 +3,8 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from database_helper import DatabaseHelper
 import sqlite3
+import asyncio
+import aiohttp
 import requests
 import re
 import os
@@ -10,6 +12,10 @@ import os
 def get_domain_from_url(url: str):
     domain = url.split('//')[-1].split('/')[0].split('?')[0].split(':')[0]
     return domain
+
+async def fetch(session, url):
+    async with session.get(url) as response:
+        return await response.text()
 
 # Папка датасета
 dataset_directory = 'dataset'
@@ -33,58 +39,38 @@ driver.set_window_size(desired_width, desired_height)
 # Подключение к базе данных
 db_helper = DatabaseHelper('database.db')
 
-def scrape_and_save_data(url: str):
-    try:
-        response = requests.get(url)
-    except:
-        print(f"Не удалось получить доступ к сайту {url}")
-        driver.quit()
-        db_helper.add_to_error(url)
-        return
-    
-    # Extract site name from URL
-    site_name = get_domain_from_url(url)
-    
-    # Create a directory for each site
-    site_directory = str(db_helper.get_last_id())
-    full_path = os.path.join(dataset_directory, site_directory)
-    os.makedirs(full_path, exist_ok=True)
-    
-
-    try:
-        #Save a screenshot
-        screenshot_filename = f"screenshot-{site_name}.png"
-        screenshot_path = os.path.join(full_path, screenshot_filename)
-        driver.get(url)
-        driver.save_screenshot(screenshot_path)
-    except:
-        print(f"Не удалось сделать скриншот {url}")
-        driver.quit()
-        db_helper.add_to_error(url)
-        return
-    
-    # Extract text from the site
-    soup = BeautifulSoup(driver.page_source, "html.parser")
-    text = soup.get_text()
-    
-    #добавить найденные URL в очередь
-    # Add the found URLs to urls_to_parse
-    links = soup.find_all('a', href=True)
-    url_array = [link['href'] for link in links if link['href'].startswith(('http://', 'https://'))]
-    add_finded_URLs(url_array)
-
-    # Save the text to a text file
-    text_filename = f"text-{site_name}.txt"
-    with open(os.path.join(full_path, text_filename), "w", encoding="utf-8") as text_file:
-        text_file.write(text)
-    
-    db_helper.write_in_DB(url, os.path.abspath(screenshot_path), text)
-
-    print(f"Сохранены данные для сайта {url} в папку {full_path}")
-    
-
-    
-
+async def scrape_and_save_data(url: str):
+    async with aiohttp.ClientSession() as session:
+        try:
+            html = await fetch(session, url)
+            soup = BeautifulSoup(html, "html.parser")
+            # Extract site name from URL
+            site_name = get_domain_from_url(url)
+            # Create a directory for each site
+            site_directory = str(db_helper.get_last_id())
+            full_path = os.path.join(dataset_directory, site_directory)
+            os.makedirs(full_path, exist_ok=True)
+            # Save a screenshot
+            screenshot_filename = f"screenshot-{site_name}.png"
+            screenshot_path = os.path.join(full_path, screenshot_filename)
+            driver.get(url)
+            driver.implicitly_wait(10)
+            driver.save_screenshot(screenshot_path)
+            # Extract text from the site
+            text = soup.get_text()
+            # Add the found URLs to urls_to_parse
+            links = soup.find_all('a', href=True)
+            url_array = [link['href'] for link in links if link['href'].startswith(('http://', 'https://'))]
+            add_finded_URLs(url_array)
+            # Save the text to a text file
+            text_filename = f"text-{site_name}.txt"
+            with open(os.path.join(full_path, text_filename), "w", encoding="utf-8") as text_file:
+                text_file.write(text)
+            
+            db_helper.write_in_DB(url, os.path.abspath(screenshot_path), text)
+            print(f"Сохранены данные для сайта {url} в папку {full_path}")
+        except Exception as e:
+            print(f"Не удалось обработать сайт {url} по причине  в папку {e}")
 
 def add_finded_URLs(links):
     # Extract and add the URLs to urls_to_parse
@@ -104,12 +90,15 @@ def get_completed_urls(directory):
 first_url = 'https://infoselection.ru/infokatalog/internet-i-programmy/internet-osnovnoe/item/90-50-samykh-poseshchaemykh-sajtov-runeta'
 db_helper.add_to_queue(first_url)
 
-while True:
-    url = db_helper.get_first_from_queue()
-    if url == None:
-        break
-    scrape_and_save_data(url)
-    db_helper.remove_from_queue(url)
+async def main():
+    while True:
+        url = db_helper.get_first_from_queue()
+        if url == None:
+            break
+        await scrape_and_save_data(url)
+        db_helper.remove_from_queue(url)
+
+asyncio.run(main())
 
 driver.quit()
 db_helper.close_connection()
