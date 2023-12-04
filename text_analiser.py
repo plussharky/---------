d1dc0ea39b5ec3cfc08cfa0887209e486f1556a0
Plusharky <Plusharky@PLUSHARKY-PC>
2023-12-04 20:35:36 +0300

    Рабочая версия текстовой нейросети

--- text_analiser.py
+++ text_analiser.py
@@ -6,6 +6,24 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+def truncate_text(texts, max_length):
+    truncated_text = []
+    for text in texts:
+        if len(text) > max_length:
+            truncated_text.append(text[:max_length])
+        else:
+            truncated_text.append(text)
+    return truncated_text
+
+
+def count_unique_words_in_texts(texts):
+    unique_words = set()
+    for text in texts:
+        words = text.split()
+        unique_words.update(words)
+    unique_words_count = len(unique_words)
+    return unique_words_count
+
 def count_words_in_texts(texts):
     words_count = 0
     for text in texts:
@@ -37,17 +55,35 @@ def batch_generator(texts, labels, batch_size):
             data_pad = pad_sequences(data, maxlen=max_len)
             yield data_pad, np.array(batch_labels)
 
+def new_batch_generator(texts, labels, batch_size):
+    for i in range(0, len(texts), batch_size):
+        batch_texts = texts[i:i+batch_size]
+        batch_labels = labels[i:i+batch_size]
+        data = tokenizer.texts_to_sequences(batch_texts)
+        data_pad = pad_sequences(data, maxlen=max_len)
+        yield data_pad, np.array(batch_labels)
+
 # Загрузка данных из CSV-файла
 legitime_texts = pd.read_csv('texts.csv', sep='\t', encoding='utf-16', dtype=str)
 phishing_texts = pd.read_csv('phishing_texts.csv', sep='\t', encoding='utf-16', dtype=str)
-
+print('текста загружены')
 # Извлечение колонки 'text' в массив
 legitime_texts_array = legitime_texts['text'].values.tolist()
 phishing_texts_array = phishing_texts['text'].values.tolist()
-
+print('текста получены')
 # Очистка массива
 legitime_texts_array = clean_array(legitime_texts_array)
 phishing_texts_array = clean_array(phishing_texts_array)
+print('текста очищены')
+# Обрезка текста
+avg_words_in_legitime_count = count_words_in_texts(legitime_texts_array) // len(legitime_texts_array)
+avg_words_in_phishing_count = count_words_in_texts(phishing_texts_array) // len(phishing_texts_array)
+
+#avg_words = (avg_words_in_legitime_count + avg_words_in_phishing_count) // 2
+avg_words = max(avg_words_in_legitime_count, avg_words_in_phishing_count)
+
+legitime_texts_array = truncate_text(legitime_texts_array, avg_words)
+phishing_texts_array = truncate_text(phishing_texts_array, avg_words)
 
 # предположим, что у нас есть некоторые данные
 texts = legitime_texts_array + phishing_texts_array
@@ -58,23 +94,26 @@ legitime_labels = [1] * legitime_count
 phishing_labels = [0] * phishing_count
 labels = legitime_labels + phishing_labels
 
-words_count = count_words_in_texts(texts)
+words_count = count_unique_words_in_texts(texts)
 max_len = count_max_len_in_texts(texts)
+avg_words_count = count_words_in_texts(texts) // len(texts)
 
+print('текста посчитаны')
 tokenizer = Tokenizer(num_words=words_count,
     filters='!–"—#$%&amp;()*+,-./:;<=>?@[\\]^_`{|}~\t\n\r«»',
     lower=True,
     split=' ',
     char_level=False)
 tokenizer.fit_on_texts(texts)
+print('токенайзер посчитан')
 
 #data = tokenizer.texts_to_sequences(texts)
 #data_pad = pad_sequences(data, maxlen=max_len)
 
 batch_size = 100  # Выберите размер батча, который подходит для вашей системы
 steps_per_epoch = len(texts) // batch_size  # Количество шагов на эпоху
-
 train_generator = batch_generator(texts, labels, batch_size)
+print('генератор посчитан')
 
 # создание модели
 model = Sequential()
@@ -86,14 +125,8 @@ model.add(Dense(1, activation='sigmoid'))
 
 # компиляция и обучение модели
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-history = model.fit(train_generator, steps_per_epoch=steps_per_epoch, epochs=20,validation_split=0.2,)
-
-plt.plot(history.history['categorical_accuracy'])
-plt.title('Model accuracy')
-plt.ylabel('Accuracy')
-plt.xlabel('Epoch')
-plt.legend(['Train'], loc='upper left')
-plt.show()
+history = model.fit(train_generator, steps_per_epoch=steps_per_epoch, epochs=20)
+#history = model.fit(train_generator, steps_per_epoch=steps_per_epoch, epochs=20, validation_split=0.2)
 
 model.save_weights('text_weights.h5') #Сохранение весов модели
 model.save('text_model.keras') #Сохранение модели
